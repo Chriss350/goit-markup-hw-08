232a368c016e74981188be3e08654ef04a075d16
Chriss350 <106681377+Chriss350@users.noreply.github.com>
2022-08-24 22:18:40 +0200

    upload

--- js/modal.js
+++ js/modal.js
@@ -3,11 +3,16 @@
         openModalBtn: document.querySelector("[data-modal-contact-open]"),
         closeModalBtn: document.querySelector("[data-modal-contact-close]"),
         contactModal: document.querySelector("[contactModal]"),
+
+        openNavBtn: document.querySelector("[data-modal-nav-open]"),
+        closeNavBtn: document.querySelector("[data-modal-nav-close]"),
         navModal: document.querySelector("[data-modal-nav]"),
     };
 
     refs.openModalBtn.addEventListener("click", SlideIn);
     refs.closeModalBtn.addEventListener("click", SlideOut);
+    refs.openNavBtn.addEventListener("click", Show);
+    refs.closeNavBtn.addEventListener("click", Hide);
 
     function SlideIn() {
         refs.contactModal.className = "modal-box in";
@@ -15,5 +20,13 @@
 
     function SlideOut() {
         refs.contactModal.classList.add("out");
+    }
+
+    function Show() {
+        refs.navModal.classList.remove("isHidden");
+    }
+
+    function Hide() {
+        refs.navModal.classList.add("isHidden");
     }
 })();
